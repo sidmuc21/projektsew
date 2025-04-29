fda0438dd595afb5ccdcf4ad56a15e50fc643161
SidritMucaj <sidmuc@htl-shkoder.com>
2025-04-29 14:17:03 +0200

    initialized

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.projektsew2025 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.projektsew2025 to javafx.fxml;
+    exports com.example.projektsew2025;
+}
